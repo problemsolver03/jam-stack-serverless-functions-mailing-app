f5edc46d19a56a31128ffcec3f247c295e9411d1
problemsolver03 <designangel2013@gmail.com>
2021-06-27 23:41:02 +0530

    added form state management using useReducer and added form submission states

--- components/Form.js
+++ components/Form.js
@@ -1,34 +1,111 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./form.css";
 
+const INTIAL_STATE = {
+  name: "",
+  email: "",
+  subject: "",
+  body: "",
+  status: "IDLE",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "updateFieldValue":
+      return { ...state, [action.field]: action.value };
+    case "updateStatus":
+      return { ...state, status: action.status };
+    case "reset":
+    default:
+      return INTIAL_STATE;
+  }
+};
+
 const Form = () => {
+  const [state, dispatch] = useReducer(reducer, INTIAL_STATE);
+
+  const udpateValue = (field) => (event) => {
+    dispatch({
+      type: "updateFieldValue",
+      field,
+      value: event.target.value,
+    });
+  };
+
+  const setStatus = (status) => dispatch({ type: "updateStatus", status });
   const submiHandler = (event) => {
     event.preventDefault();
+    console.log(state);
+    setStatus("PENDING");
   };
+
+  if (state.status === "SUCCESS") {
+    return (
+      <p className="success">
+        Message sent successfully!
+        <br />
+        <button className="reset" onClick={() => dispatch({ type: "reset" })}>
+          Reset
+        </button>
+      </p>
+    );
+  }
   return (
-    <form className="form" onSubmit={submiHandler}>
-      <h3>Send Mail Form</h3>
-      <label className="label">
-        Name
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Subject
-        <input type="text" name="subject" />
-      </label>
-      <label>
-        Body
-        <textarea name="name" />
-      </label>
+    <>
+      {state.status === "ERROR" && (
+        <p className="error">
+          Something went wrong <br /> please try again.
+        </p>
+      )}
+      <form className="form" onSubmit={submiHandler}>
+        <h3>Send Mail Form</h3>
+        <label className="label">
+          Name
+          <input
+            type="text"
+            name="name"
+            value={state.name}
+            onChange={udpateValue("name")}
+          />
+        </label>
+        <label>
+          Email
+          <input
+            type="email"
+            name="email"
+            value={state.email}
+            onChange={udpateValue("email")}
+          />
+        </label>
+        <label>
+          Subject
+          <input
+            type="text"
+            name="subject"
+            value={state.subject}
+            onChange={udpateValue("subject")}
+          />
+        </label>
+        <label>
+          Body
+          <textarea
+            name="body"
+            value={state.body}
+            onChange={udpateValue("body")}
+          />
+        </label>
 
-      <br />
+        <br />
 
-      <button className="submitButton">Send Mail</button>
-    </form>
+        <button
+          type="submit"
+          className="submitButton"
+          disabled={state.status === "PENDING" ? true : false}
+        >
+          {state.status === "PENDING" ? "Please wait ..." : "Send Mail"}
+        </button>
+      </form>
+    </>
   );
 };
 
